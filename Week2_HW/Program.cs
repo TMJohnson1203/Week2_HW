aff0cca438b37d6bb592d36f8a83743d61d84825
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-16 15:15:06 -0400

    initial commit

---
+++ Week2_HW/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ARRAYS
+
+            //  string[] vacaySpots;
+            //  string[] vacaySpots = new string[];
+
+            //  string[0] "Italy";
+
+            //{ "Italy", "France", "Spain" };
+            //  Console.WriteLine(vacaySpots + vacaySpots);
+
+
+            //// LOOPS
+
+
+            string[] dayofweek = { "monday", "tuesday", "wednesday" };
+            Console.WriteLine(dayofweek[]);
+
+
+
+
+
+
+
+        }
+    }
+}
